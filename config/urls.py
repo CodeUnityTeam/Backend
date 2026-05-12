726afaf10f0f6b6293f7844ef7b4796efb6fce65
Sergey Khoetskiy <khoetskiy@gmail.com>
2026-05-12 20:36:06 +0200

    Remove duplicate urlpatterns in urls.py

--- config/urls.py
+++ config/urls.py
@@ -8,9 +8,3 @@ urlpatterns = [
     path('api/v1/', include('users.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/', include('users.urls')),
-    path('api-auth/', include('rest_framework.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
