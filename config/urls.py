dc7a92c76dcf181aa53423f2b9bc2d528ba6be5b
OlegMiskhozhev <omiskhozhev@gmail.com>
2026-05-13 07:58:46 +0300

    Add standart and social auth settings

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import JsonResponse
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from datetime import datetime
-
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path(
+        "api/v1/",
+        include(
+            [
+                path("auth/", include("users.urls")),
+            ]
+        ),
+    ),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
