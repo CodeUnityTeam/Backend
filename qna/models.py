be79114061feed23f4077ae8348dc3e166442c19
Станислав Баринов <152168070+hixwizard@users.noreply.github.com>
2026-06-16 17:08:21 +0300

    убрал лишнее поле

--- qna/models.py
+++ qna/models.py
@@ -122,10 +122,6 @@ class Answer(models.Model):
         auto_now_add=True,
         verbose_name='Дата создания',
     )
-    likes_count = models.IntegerField(
-        default=0,
-        verbose_name='Количество лайков',
-    )
 
     class Meta:
         """Метаданные модели."""
