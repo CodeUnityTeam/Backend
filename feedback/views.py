a5af515bfbdd9dddba65497999c44df71f42bbf3
Aleksandr_Bystrov <bystrovalse@yandex.ru>
2026-06-26 16:08:49 +0300

    Исправил замечания линтера

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,5 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import status, viewsets
-from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -30,4 +29,4 @@ class FeedbackViewSet(viewsets.ModelViewSet):
         print('data = ', request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(status=status.HTTP_201_CREATED,)
+        return Response(status=status.HTTP_201_CREATED)
