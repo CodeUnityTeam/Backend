877cd6cd696b5638f40b350a4aacf286cd760565
hixwizard <stanislavbarinov0@gmail.com>
2026-06-27 12:54:37 +0300

    настройка email

--- config/settings.py
+++ config/settings.py
@@ -282,7 +282,7 @@ ACCOUNT_USER_MODEL_USERNAME_FIELD = None
 DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER', 'hello@code-unity.ru')
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
 
-if os.getenv('PRODUCTION_EMAIL_BACKEND', False) is True:
+if os.getenv('PRODUCTION_EMAIL_BACKEND', 'False').lower() == 'true':
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_HOST = os.getenv('EMAIL_HOST')
     EMAIL_PORT = int(os.getenv('EMAIL_PORT', 465))
