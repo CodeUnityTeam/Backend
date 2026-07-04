b56ae25fbe10eef3a28bb6be53aede0791708424
Viacheslav Shipov <pinky887@yandex.ru>
2026-07-05 00:00:58 +0300

    Правки разрешений в QNA

--- qna/permissions.py
+++ qna/permissions.py
@@ -3,22 +3,21 @@ from typing import Any
 from rest_framework import permissions
 from rest_framework.request import Request
 
-from qna.models import Answer
+from qna.models import Answer, Question
 
 
-class CanDeleteAnswer(permissions.BasePermission):
-    """Разрешает удаление ответа только автору, модератору или админу."""
+class CanUpdateDeleteQNA(permissions.BasePermission):
+    """Разрешает удаление/обновление автору, модератору или админу."""
 
     def has_object_permission(
         self,
         request: Request,
         view: Any,
-        obj: Answer,
+        obj: Answer | Question,
     ) -> bool:
         """Проверяет, может ли пользователь удалить ответ."""
-        if request.method != 'DELETE':
+        if request.method in permissions.SAFE_METHODS:
             return True
-
         user = request.user
         return (
             obj.user == user
