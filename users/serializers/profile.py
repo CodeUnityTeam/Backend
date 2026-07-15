770a4e6343b79780b934f8ec93c8e8492a14a2b4
Евгений Цыганков <evgeniyts07@gmail.com>
2026-07-15 21:09:30 +0300

    Revert "Add last_login field in profile serializres"
    This reverts commit 8fa06a797d51586942ed98eae0596c2da36bc058.

--- users/serializers/profile.py
+++ users/serializers/profile.py
@@ -112,7 +112,6 @@ class MeProfileUpdateSerializer(UserDetailsSerializer):
             'skills',
             'specializations',
             'workformats',
-            'last_login',
         )
 
     def update(self, instance: Any, validated_data: dict) -> Any:
@@ -220,15 +219,13 @@ class DetailUserProfileSerializer(PublicUserProfileSerializer):
         many=True, read_only=True,
     )
 
-    class Meta:
-        model = UserModel
+    class Meta(PublicUserProfileSerializer.Meta):
         fields = PublicUserProfileSerializer.Meta.fields + (
             'country',
             'role',
             'soft_skills',
             'about_me',
             'experiences',
-            'last_login',
         )
         read_only_fields = fields
 
