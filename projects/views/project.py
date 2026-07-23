8b823d571a778463d05c63cdd7ebeb5302dc6090
hixwizard <stanislavbarinov0@gmail.com>
2026-07-23 18:41:55 +0300

    исправил 500 в проектах

--- projects/views/project.py
+++ projects/views/project.py
@@ -200,7 +200,7 @@ class ProjectViewSet(CacheRetrieveMixin, ModelViewSet):
                 serializer = self.get_serializer(page, many=True)
                 logger.debug(
                     'Cache HIT: страница %d, элементов на странице %d',
-                    page.number, len(page),
+                    self.page.number, len(page),
                 )
                 return self.get_paginated_response(serializer.data)
             serializer = self.get_serializer(qs, many=True)
