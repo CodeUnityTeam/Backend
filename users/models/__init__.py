624c6661b465b056039f6f46a617e470d1f36ebd
Viacheslav Shipov <pinky887@yandex.ru>
2026-07-28 18:30:30 +0300

    убран импорт

--- users/models/__init__.py
+++ users/models/__init__.py
@@ -1,5 +1,3 @@
-from projects.models.work_format import WorkFormat
-
 from .skills import Skill, UserSkill
 from .specializations import Specialization, UserSpecialization
 from .users import User, UserExperience, UserLike
