dcf39b2c057166afb5608c431eb8538d3de37133
Kristina Alabova <kristina.alabova@mail.ru>
2026-08-03 15:30:50 +0300

    Fix my-project-filter

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Third-party packages
+    'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
     'drf_spectacular',
