f02b7e2d599a7d8487d1fd13227b4590a737cda4
Kristina Alabova <kristina.alabova@mail.ru>
2026-08-03 12:18:30 +0300

    Добавлена аннотация поля likes_count для ответа в get_question_detail_queryset

--- qna/selectors.py
+++ qna/selectors.py
@@ -61,7 +61,11 @@ def get_question_detail_queryset() -> QuerySet[Question]:
             'answers',
             queryset=Answer.objects.select_related(
                 'user',
-            ).prefetch_related('images', 'likes').filter(is_active=True),
+            ).prefetch_related('images', 'likes').filter(
+                is_active=True,
+            ).annotate(
+                likes_count=Count('likes', distinct=True),
+            ),
         ),
         'likes',
         'images',
